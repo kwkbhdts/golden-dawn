31c7e1ff5aa796c2be6902d41dcaa91c19a9b43a
Hidetoshi <kwkbhdts@gmail.com>
2023-03-27 00:02:57 +0900

    Add features

--- src/daily_directory.rs
+++ src/daily_directory.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::fs::DirEntry;
 use std::io;
 use std::path::Path;
 use std::path::PathBuf;
@@ -26,9 +27,35 @@ impl<'a> DailyDirectory<'a> {
         }
     }
 
+    pub fn search_daily_directories(
+        dir_path: &'a Path,
+        date_format: &'a String
+    ) -> io::Result<Vec<DailyDirectory<'a>>> {
+        let mut daily_directory_vec = Vec::new();
+        let read_dir = fs::read_dir(dir_path)?;
+        for dir_entry_result in read_dir {
+            let dir_entry = dir_entry_result?;
+            if let Some(daily_directory) =
+                Self::from_dir_entry(dir_path, date_format, dir_entry) {
+                daily_directory_vec.push(daily_directory);
+            }
+        }
+        Ok(daily_directory_vec)
+    }
+
+    /// Returns whether this DailyDirectory is old.
+    pub fn is_old(&self, days_to_old: u32) -> bool {
+        if days_to_old == 0 {
+            return false;
+        }
+        let today = Local::now().naive_utc().date();
+        let elapsed_days = (today - self.date).num_days();
+        elapsed_days >= (days_to_old as i64)
+    }
+
     /// Create a directory of this DailyDirectory.
     pub fn create(&self) -> io::Result<()> {
-        fs::create_dir_all(self.create_dir_path())
+        fs::create_dir_all(&self.create_dir_path())
     }
 
     /// Create a junction of this DailyDirectory.
@@ -46,9 +73,50 @@ impl<'a> DailyDirectory<'a> {
         junction::create(self.create_dir_path(), junction_path)
     }
 
+    /// Move directory of this DailyDirectory.
+    pub fn move_to(&self, new_parent_dir_path: &'a Path) -> io::Result<()> {
+        let src = self.create_dir_path();
+        let dir_name = self.date.format(self.date_format).to_string();
+        let dst = new_parent_dir_path.join(dir_name);
+        fs::rename(src, dst)
+    }
+
+    /// Remove directory of this DailyDirectory.
+    pub fn remove(&self) -> io::Result<()> {
+        fs::remove_dir_all(self.create_dir_path())
+    }
+
     /// Create a dir path of this DailyDirectory.
-    fn create_dir_path(&self) -> PathBuf {
+    pub fn create_dir_path(&self) -> PathBuf {
         let dir_name = self.date.format(self.date_format).to_string();
         self.parent_dir_path.join(dir_name)
+    }
+
+    // Create a DailyDirectory from a DirEntry.
+    fn from_dir_entry(
+        dir_path: &'a Path,
+        date_format: &'a String,
+        dir_entry: DirEntry
+    ) -> Option<DailyDirectory<'a>> {
+        // Return None if dir_entry is not a directory.
+        if let Ok(metadata) = dir_entry.metadata() {
+            if !metadata.is_dir() {
+                return None;
+            }
+        } else {
+            return None;
+        }
+
+        // Create and return a DailyDirectory.
+        if let Ok(daily_dir_name) = dir_entry.file_name().into_string() {
+            if let Ok(date) = NaiveDate::parse_from_str(&daily_dir_name, date_format) {
+                return Some(DailyDirectory {
+                    parent_dir_path: dir_path,
+                    date_format: date_format,
+                    date: date,
+                });
+            }
+        }
+        None
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,64 @@
+use std::fs;
 use std::path::Path;
 
 use config::Config;
 use daily_directory::DailyDirectory;
 
+mod debug;
 mod config;
 mod daily_directory;
 
+
 const CONFIG_FILE_PATH: &str = "resource/config.toml";
 const TODAY_JUNCTION: &str = "today";
+const OLD_DIR_NAME: &str = "old";
 
 fn main() {
     // Load the config file.
     let config = Config::from_file(&CONFIG_FILE_PATH.to_string())
         .expect("Failed to load a config file.");
-    println!("config = {:?}", config);
+    debug!(println!("config = {:?}", config));
 
     // Create a directory of today.
-    let dir_of_today = DailyDirectory::direcory_of_today(
-        Path::new(&config.parent_dir_path),
-        &config.date_format);
-    println!("dir_of_today = {:?}", dir_of_today);
+    let parent_dir_path = Path::new(&config.parent_dir_path);
+    let date_format = &config.date_format;
+    let dir_of_today = DailyDirectory::direcory_of_today(parent_dir_path, date_format);
+    debug!(println!("dir_of_today = {:?}", dir_of_today));
     dir_of_today.create()
         .expect("Failed to create a directory of today.");
 
     // Create a junction of today.
     dir_of_today.create_junction(Path::new(TODAY_JUNCTION))
-        .expect("Failed to create a junction of today.")
+        .expect("Failed to create a junction of today.");
+
+    // Get daily directories.
+    let daily_directories =
+        DailyDirectory::search_daily_directories(parent_dir_path, date_format)
+            .expect("Failed to search daily directoris.");
+    debug!(println!("daily_directories = {:?}", daily_directories));
+
+    // Move old directories.
+    let old_dir_path = parent_dir_path.join(OLD_DIR_NAME);
+    for x in daily_directories {
+        if x.is_old(config.days_to_move) {
+            fs::create_dir_all(&old_dir_path)
+                .unwrap_or_else(|_| panic!("Failed to create \"{}\" directory.", OLD_DIR_NAME));
+            x.move_to(&old_dir_path)
+                .expect("Failed to move a daily directory.")
+        }
+    }
+
+    // Get old daily directories.
+    let old_daily_directories =
+        DailyDirectory::search_daily_directories(&old_dir_path, date_format)
+            .expect("Failed to search old daily directoris.");
+    debug!(println!("old_daily_directories = {:?}", old_daily_directories));
+
+    // Remove more older directories.
+    for x in old_daily_directories {
+        if x.is_old(config.days_to_remove) {
+            x.remove().expect("Failed to remove a old daily directory.");
+        }
+    }
+
 }
