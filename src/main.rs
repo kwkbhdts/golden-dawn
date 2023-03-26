12cca5be8a924476abc972686c27bb0d1dcbbae0
Hidetoshi <kwkbhdts@gmail.com>
2023-03-26 17:54:17 +0900

    Add Config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod config;
+
+use config::Config;
+
+const CONFIG_FILE_PATH: &str = "resource/config.toml";
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::from_file(&CONFIG_FILE_PATH.to_string())
+        .expect("Failed to load a config file.");
+    println!("{:?}", config);
 }
