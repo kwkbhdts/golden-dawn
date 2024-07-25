a5a165ffdf51cbe5a1079a339ddeb1641cd235a2
Hidetoshi <kwkbhdts@gmail.com>
2024-07-25 21:22:59 +0900

    Modify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use chrono::{Datelike, Local, NaiveDate};
+
 fn main() {
-    println!("Hello, world!");
+    let today = Local::now().date_naive();
+    create_direcory_of(today)
 }
+
+fn create_direcory_of(date :NaiveDate) {
+    println!("{}-{:02}-{:02}", date.year(), date.month(), date.day())
+}
+
