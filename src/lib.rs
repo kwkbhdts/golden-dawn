da13450d7ea91204f9574ebd92e65997c9e04ffa
Hidetoshi <kwkbhdts@gmail.com>
2024-07-28 21:30:54 +0900

    Add log and make it possible to create directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! golden-dawn : Daily Directory Maker
 
 // Standard libraries ------------------
+use std::path::Path;
 // Cargo libraries ----------------------
 use chrono::{Datelike, Local, NaiveDate};
 // Modules ------------------------------
@@ -9,8 +10,24 @@ mod util;
 
 /// The main function of golden-dawn.
 pub fn main() {
+    // Init logger
+    env_logger::init();
+
+    // Get target directory path
     let today = Local::now().date_naive();
-    println!("{}", get_dir_name_of(today))
+    let current_dir = Path::new(".").to_path_buf();
+    let mut target_dir_path = util::get_dir_path_of_exe(&current_dir);
+    target_dir_path.push(get_dir_name_of(today));
+    log::info!("target_path = {}", target_dir_path.to_str().unwrap());
+
+    // Create target directory
+    match util::create_dir(&target_dir_path) {
+        Ok(_) => (),
+        Err(e) => log::error!(
+            "Couldn't create the directory. ({}) : {}",
+            target_dir_path.to_str().unwrap(),
+            e.to_string())
+    }
 }
 
 /// It converts a NaiveDate to a direcory name string.
